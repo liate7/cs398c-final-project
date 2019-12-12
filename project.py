11bc77c45308b7ed1d06dd83d2d8660908f9afad
Andrew Patterson <patterson_a@lynchburg.edu>
2019-12-11 22:07:52 -0500

    Everything working; hopefully the next change will speed things up a lot
    Signed-off-by: Andrew Patterson <patterson_a@lynchburg.edu>

--- project.py
+++ project.py
@@ -3,11 +3,9 @@ import nltk
 import pandas
 import pickle
 import os
-import functools
-from nltk.corpus import sentiwordnet
 import collections
-import numpy
-import math
+
+import recommender
 #- UI
 #   - Like previous assignments
 #   - Ask if indexing (LSA (3 versions???), doc2vec, maybe repr for recommender part)
@@ -51,7 +49,7 @@ def get_index():
 ## Call all your indexing functions in here.
 def index():
     """ Function to do all the indexing, returns the index after writing it """
-    recommender_index()
+    recommender.index()
 
 def dispatch():
     PROMPT = "[Search] by title, search by title in [genre], [recommend] a movie, or [quit]? "
@@ -75,109 +73,6 @@ def dispatch():
     else:
         print('Respond with a prefix of one of search, genre, recommend, or quit')
         return False
-
-def recommender_index(moviesDF):
-    """
-    Generate the index for the recommender
-    Takes in a pandas thing of moviesDF.csv
-    """
-    # Get TF-IDF scores
-    weights = tfidf_scores(moviesDF['reviewText'])
-    # Get the scores
-    ratings = ratings_matrix(moviesDF, weights)
-    #for reviewer in m['reviewerID'].unique():
-    return ratings
-
-def tfidf_scores(docs_iter):
-    """
-    Takes in an iterator of documents
-    Returns a dict{word, tf-idf score}
-    """
-    term_freqs = dict()
-    doc_freqs = dict()
-    num_docs = 0
-    for doc in docs_iter:
-        words = score_doc(doc, term_freqs)
-        for word in words:
-            doc_freqs[word] = doc_freqs.get(word, 0) + 1
-        num_docs += 1
-    scores = term_freqs
-    for key in scores:
-        scores[key] /= math.log(num_docs / doc_freqs[key])
-    return scores
-
-def score_doc(doc, freq_dict):
-    """
-    Take in a string and a dict to put word frequencies in
-    Returns a set of words in the document
-    """
-    ret = set()
-    if isinstance(doc, str):
-        for word in nltk.word_tokenize(doc):
-            ret.add(word)
-            freq_dict[word] = freq_dict.get(word, 0) + 1
-    return ret
-
-def ratings_matrix(moviesDF, weights):
-    """
-    Takes in a pandas thing with reviewerID, movieID, and reviewText parts
-    Returns a [max(reviewerIDs), max(movieID)] array of reviewTexts
-    """
-    ret = [[""] * moviesDF['movieID'].max()] * moviesDF['reviewerID'].max()
-    def closure(row):
-        """
-        Is a named function because python doesn't allow assignment in lambdas
-        """
-        if isinstance(row['reviewText'], str):
-            ret[row['reviewerID'] - 1][row['movieID'] - 1] = process_text(row['reviewText'], weights)
-        else:
-            ret[row['reviewerID'] - 1][row['movieID'] - 1] = 0
-    moviesDF.apply(closure, axis=1)
-    return ret
-
-
-def process_text(text, weights):
-    sents = map(nltk.word_tokenize, nltk.sent_tokenize(text))
-    tagged = nltk.pos_tag_sents(sents)
-    ret = avg([ avg([tag_tuple(x) * weights.get(x[0], 0) for x in sent if x[0] not in nltk.corpus.stopwords.words('english') ])
-            for sent in tagged])
-    # return list(flatten(ret))
-    return ret
-
-def flatten(it):
-    for x in it:
-        if isinstance(x, str):
-            yield x
-        elif isinstance(x, collections.Iterable):
-            for y in flatten(x):
-                yield y
-        else:
-            yield y
-
-def upenn_to_wn_tag(tag):
-    transl = { 'JJ' : 'a', 'RB' : 'r', 'NN' : 'n', 'VB' : 'v' }
-    if len(tag) >= 2:
-        return transl.get(tag[:2], None)
-    elif len(tag) == 1:
-        tag
-
-def tag_tuple(tupl):
-    def weight(synset):
-        return synset.pos_score() - synset.neg_score()
-    word, tag = tupl
-    synsets = sentiwordnet.senti_synsets(word, upenn_to_wn_tag(tag))
-    return avg([weight(s) for s in synsets])
-
-def avg(it):
-    i = 0
-    acc = 0
-    for x in it:
-        acc += x
-        i += 1
-    if i == 0:
-        return 0
-    else:
-        return acc / i
 
 def ask(prompt):
     res = input(prompt)
---
+++ recommender.py
@@ -0,0 +1,146 @@
+from nltk.corpus import sentiwordnet
+import numpy as np
+import pandas
+import nltk
+
+def index(moviesDF):
+    """
+    Generate the index for the recommender
+    Takes in a pandas thing of moviesDF.csv
+    """
+    tagged = tag_docs(moviesDF['reviewText'])
+    lemmas, docs = lemmatize(tagged)
+    matrix = make_matrix(list(lemmas), docs)
+    idfs = np.log(matrix.shape[1] / np.count_nonzero(matrix, 0))
+    tfidf = np.apply_along_axis(lambda row: (row / np.sum(row)) * idfs, 1, matrix)
+    # list of (average) sentiments per lemma, tag
+    lemma_sentiments = np.array( [tag_tuple(x) for x in lemmas] )
+    # doc -> weight array!
+    doc_sentiments = np.apply_along_axis(lambda row: np.sum(row * lemma_sentiments),
+            1, tfidf)
+    # matrix is 0-based, while dataframe is 1-based, so indices are ID-1, not ID
+    scores_matrix = doclist_to_matrix(moviesDF, doc_sentiments)
+    similarities = cosine_sim_table(np.nan_to_num(scores_matrix))
+    # Gets a matrix vectors [ sum(sentiment_i * weight_i) foreach movie ]
+    weights = np.nan_to_num(scores_matrix).dot(similarities)
+    indices_sorted = np.fliplr(np.argsort(weights))
+    # Finally, return a list of recommendations for each user
+    return recommended_matrix(indices_sorted, scores_matrix)
+
+def tag_docs(dociter):
+    """
+    Take in an iterable of documents
+    Returns an iterable of the documents POS-tagged with wordnet tags
+    """
+    for doc in dociter:
+        tagged = []
+        if isinstance(doc, str):
+            tagged = nltk.pos_tag(nltk.word_tokenize(doc))
+        yield list([(word[0], upenn_to_wn_tag(word[1])) for word in tagged])
+
+def lemmatize(dociter):
+    """
+    Takes in an iterable of tagged documents
+    Returns a set of all the lemmas and a list of the documents lemmatized
+    """
+    lemmas = set()
+    documents = list()
+    wn = nltk.WordNetLemmatizer()
+    for doc in dociter:
+        lemmatized = []
+        for word, tag in doc:
+            lemma = ( wn.lemmatize(word, tag), tag )
+            lemmatized.append(lemma)
+            lemmas.add(lemma)
+        documents.append(lemmatized)
+    return lemmas, documents
+
+def make_matrix(lemmas, docs):
+    """
+    Takes an list of lemmas and a list of lemmatized docs
+    Returns a num(docs) by num(lemmas) matrix of word counts
+    """
+    matrix = np.zeros((len(docs), len(lemmas)))
+    doclen = len(docs)
+    lemmalen = len(lemmas)
+    for doc in range(doclen):
+        for lemma in range(lemmalen):
+            matrix[doc][lemma] = docs[doc].count(lemmas[lemma])
+    return matrix
+
+def doclist_to_matrix(moviesDF, doc_sent):
+    """
+    Takes in a pandas dataframe with reviewerID and movieID columns and a list
+    of sentiments
+    Returns a matrix(reviewerID, movieID) of sentiments
+    """
+    ret = np.full((moviesDF['reviewerID'].max(), moviesDF['movieID'].max()), np.nan)
+    for i in range(len(doc_sent)):
+        ret[moviesDF['reviewerID'][i] - 1][moviesDF['movieID'][i] - 1] = doc_sent[i]
+    return np.array(ret)
+
+def upenn_to_wn_tag(tag):
+    transl = { 'JJ' : 'a', 'RB' : 'r', 'NN' : 'n', 'VB' : 'v' }
+    if len(tag) >= 2:
+        return transl.get(tag[:2], 'n')
+    else:
+        return 'n'
+
+def tag_tuple(tupl):
+    def weight(synset):
+        return synset.pos_score() - synset.neg_score()
+    word, tag = tupl
+    synsets = sentiwordnet.senti_synsets(word, upenn_to_wn_tag(tag))
+    return avg([weight(s) for s in synsets])
+
+def avg(it):
+    i = 0
+    acc = 0
+    for x in it:
+        acc += x
+        i += 1
+    if i == 0:
+        return 0
+    else:
+        return acc / i
+
+def cos_sim(u, v):
+    '''
+    Takes 2 vectors u, v and returns the cosine similarity according 
+    to the definition of the dot product
+    '''
+    dot_product = np.dot(u, v)
+    norm_u = np.linalg.norm(u) # magnitude of u
+    norm_v = np.linalg.norm(v) # magnitude of v
+    return dot_product / (norm_u * norm_v)
+
+def cosine_sim_table(matrix):
+    '''
+    Takes in a matrix of shape (x,y)
+    Returns a matrix of shape (y,y) of the cosine similarities in a table
+    '''
+    # This code is from <https://stackoverflow.com/a/37929303>
+    # It only halfway makes sense to me, I don't really have the linear algebra
+    # knowledge to get it, but it should be faster than a looping version
+    #
+    # cos(v1,v2) = (v1 `dot` v2) / (norm(v1) * norm(v2))
+    # matrix.T `dot` matrix -> matrix of dot products
+    # norm is vector of normalized arrays
+    norm = np.linalg.norm(matrix, axis=0)[None, :]
+    return matrix.T.dot(matrix) / norm.T.dot(norm)
+
+def recommended_matrix(sorted_indices, sentiments_matrix):
+    '''
+    Take in a matrix of indices sorted in most recommended-least recommended, and a matrix of sentiments
+    Returns the indices of the first five that are nan (not reviewed) in the sentiments matrix
+    '''
+    ret = np.full((sorted_indices.shape[0], 5), np.nan)
+    for user in range(sorted_indices.shape[0]):
+        i = 0
+        for index in sorted_indices[user]:
+            if np.isnan(sentiments_matrix[user][index]):
+                ret[user][i] = index
+                i += 1
+                if i == 5:
+                    break
+    return ret
